2943d90601c9d15a88c4b4d9c01ed932cf0da12a
LeonardoFloresCESUSC <1340996@faculdadecesusc.edu.br>
2024-06-24 18:25:11 -0300

    f
    funciona

--- funcoes.py
+++ funcoes.py
@@ -1,3 +1,5 @@
+import json
+
 #Função Cabeçalho
 def cabecalho():
 #cabecalho
@@ -50,27 +52,57 @@ def menu():
 #Função Cadastrar
 def cadastrar():
     try:
-    #Abrir o arquivo 'Dados.txt' em modo de apêndice (adicionar dados ao final do arquivo)
-        with open('Dados.txt', 'a') as arquivo:
-         #Solicitar ao usuário as informações do aluno
-            nomealuno = input('Informe o nome do(a) aluno(a): ')    
-            idadealuno = input('Informe a idade do(a) aluno(a): ')
-            sexoaluno = input('Informe o sexo do(a) aluno(a) (masculino ou feminino): ')
-            nomeresponsavel = input('Informe o nome do(a) responsável pelo(a) aluno(a): ')
-            telefone = input('Informe o telefone do(a) responsável: ')
-            cpfresponsavel = input('Informe o CPF do(a) responsável: ')
-            endereco = input('Informe seu endereço: ')
-            cidade = input('Informe sua cidade: ')
-            estado = input('Informe seu Estado: ') 
-            cep = input('Informe o seu CEP: ')
-          
-            #Formatar os dados do aluno em uma string
-            aluno = f'{nomealuno},{idadealuno},{sexoaluno},{nomeresponsavel},{telefone},{cpfresponsavel},{endereco},{cidade},{estado},{cep}\n'
-            #Escrever a string formatada no arquivo 'Dados.txt'
-            arquivo.write(aluno)
-            #Exibir uma mensagem de sucesso
-            print('*'*44)
-            print('Dados do(a) aluno(a) cadastrados com sucesso!')
+        #Solicitar ao usuário as informações do aluno
+        nomealuno = input('Informe o nome do(a) aluno(a): ')    
+        idadealuno = input('Informe a idade do(a) aluno(a): ')
+        sexoaluno = input('Informe o sexo do(a) aluno(a) (masculino ou feminino): ')
+        nomeresponsavel = input('Informe o nome do(a) responsável pelo(a) aluno(a): ')
+        telefone = input('Informe o telefone do(a) responsável: ')
+        cpfresponsavel = input('Informe o CPF do(a) responsável: ')
+        endereco = input('Informe seu endereço: ')
+        cidade = input('Informe sua cidade: ')
+        estado = input('Informe seu Estado: ') 
+        cep = input('Informe o seu CEP: ')
+
+        alunos=pegardados()
+        print(alunos)
+        #Gerador de IDs
+        #Pega o maior ID e adiciona 1       
+        alunoid=0
+        if alunos:
+            for id in alunos.keys():
+                if int(id)>int(alunoid):
+                    alunoid=id
+        alunoid=int(alunoid)+1
+
+        print(alunoid)
+        #Adiciona o ID gerado na chave json
+        alunos[str(alunoid)]={
+                'Nome do aluno' :nomealuno,
+                'Idade do aluno': idadealuno,
+                'Sexo do aluno': sexoaluno,
+                'Nome do responsável': nomeresponsavel,
+                'Telefone do responsável': telefone,
+                'CPF do responsável': cpfresponsavel,
+                'Endereço': endereco,
+                'Cidade': cidade,
+                'Estado': estado,
+                'CEP':cep
+        
+                }
+        
+        with open('Dados.txt', 'w') as arquivo:
+            json.dump(alunos, arquivo)
+        
+
+        #Exibir uma mensagem de sucesso
+        print('*'*44)
+        print()
+        print('Dados do(a) aluno(a) cadastrados com sucesso!')
+        print('O ID do aluno ', nomealuno, ' é: ', alunoid)
+        print()
+        print('*'*44)
+        print()
     except ValueError:
         #Capturar um erro caso os valores fornecidos pelo usuário não estejam no formato esperado
         print('Valor inválido. Verifique as informações e tente novamente')
@@ -78,54 +110,101 @@ def cadastrar():
         #Capturar qualquer outro erro que possa ocorrer durante o cadastro dos dados
         print('Ocorreu um erro ao cadastrar os dados:', str(e))
 
+#Função pegar os dados do arquivo json
+def pegardados():
+    try:
+        with open('Dados.txt', 'r') as arquivo:
+        # Inicializar uma lista para armazenar os dados dos alunos
+            return json.load(arquivo)
+    except Exception as e:
+        return {}
 
-# #Função Listar
-# def listar():
-#     try:
-#     #Abrir o arquivo 'Dados.txt' em modo de leitura ('r')
-#         with open('Dados.txt', 'r') as arquivo:
-#         #Ler todas as linhas do arquivo e armazená-las em uma lista
-#             linhas = arquivo.readlines()
-#     #Verificar se não há linhas (dados) no arquivo
-#             if not linhas:
-#                 print('Nenhum dado de aluno cadastrado,')
-#             else:
-#         #Exibir os dados do aluno na tela
-#                 print('Nome: ', nome)
-#                 print('Idade: ', idade)
-#                 print('*'*40)
-#     except FileNotFoundError:
 
+# Função Listar
+def listar():
+    try:
+        alunos= pegardados()
+        # Ler cada linha do arquivo
+        for alunoid, dados in alunos.items():
+
+            # Imprimir os dados dos alunos
+            print()
+            print('*' * 40)
+            print('Aluno ID: ', alunoid)
+            print('Nome do aluno: ', dados['Nome do aluno'])
+            print('Idade do aluno: ', dados ['Idade do aluno'])
+            print('Sexo do aluno: ', dados['Sexo do aluno'])
+            print('Nome do responsável: ', dados['Nome do responsável'])
+            print('Telefone do responsável: ', dados['Telefone do responsável'])
+            print('CPF do responsável: ', dados['CPF do responsável'])
+            print('Endereço: ', dados['Endereço'])
+            print('Cidade: ', dados['Cidade'])
+            print('Estado: ', dados['Estado'])
+            print('CEP: ', dados['CEP'])
+            print('*' * 40)
+            print()
+
+    except FileNotFoundError:
+        print("Arquivo 'Dados.txt' não encontrado.")
+    except Exception as e:
+        print(f"Ocorreu um erro: {e}")
+
+    
 
 # #Função Alterar Dados
-# def alterar():
-#     #Abrir o arquivo 'Dados.txt' em modo de leitura ('r')
-#     with open('Dados.txt', 'r') as arquivo:
-#         #Ler todas as linhas do arquivo e armazená-las em uma lista
-#         linhas= arquivo.readlines()
-#         #Verificar se não há linhas (dados) no arquivo
-#         if not linhas:
-#         print('Nenhum dado de aluno cadastrado,')
-#         return
-#         #Solicitar ao usuário o nome do aluno que deseja alterar
-#         if int(dados[0]) == nome
-#         #precisa finalizar
+def alterar():
+    alunos=pegardados()
+
+    for alunoid, dados in alunos.items():
+        print(alunoid, " ", dados['Nome do aluno'])
+
+    alteraraluno = input('Digite o ID do aluno: ')
+    print('ID selecionado: ', alteraraluno)
+
+    existAluno = False
+    for alunoid, dados in alunos.items():
+        if int(alunoid) == int(alteraraluno):
+            existAluno = True
+        
+    if existAluno:
+        nomealuno = input('Informe o nome do(a) aluno(a): ')    
+        idadealuno = input('Informe a idade do(a) aluno(a): ')
+        sexoaluno = input('Informe o sexo do(a) aluno(a) (masculino ou feminino): ')
+        nomeresponsavel = input('Informe o nome do(a) responsável pelo(a) aluno(a): ')
+        telefone = input('Informe o telefone do(a) responsável: ')
+        cpfresponsavel = input('Informe o CPF do(a) responsável: ')
+        endereco = input('Informe seu endereço: ')
+        cidade = input('Informe sua cidade: ')
+        estado = input('Informe seu Estado: ') 
+        cep = input('Informe o seu CEP: ')
+
+        alunos[str(alteraraluno)]={
+                'Nome do aluno' :nomealuno,
+                'Idade do aluno': idadealuno,
+                'Sexo do aluno': sexoaluno,
+                'Nome do responsável': nomeresponsavel,
+                'Telefone do responsável': telefone,
+                'CPF do responsável': cpfresponsavel,
+                'Endereço': endereco,
+                'Cidade': cidade,
+                'Estado': estado,
+                'CEP':cep
+        
+                }
+        
+        with open('Dados.txt', 'w') as arquivo:
+            json.dump(alunos, arquivo)
+        
+
+    else:
+        print("Aluno seleconado n existe.")
+            
 
 
 # #Função Excluir Registro
 # def excluir():
-#     #Abrir o arquivo 'Dados.txt' em modo de leitura ('r')
-#     with open('Dados.txt', 'r') as arquivo:
-#         #Ler todas as linhas do arquivo e armazená-las em uma lista
-#         linhas= arquivo.readlines()
-#         #Verificar se não há linhas (dados) no arquivo
-#         if not linhas:
-#         print('Nenhum dado de aluno cadastrado,')
-#         return
-#     nome= str
+#ao inves do dump usar o outro codigo para tirarr o json
 
-#     with open('Dados.txt','w') as arquivo:
-
-
+#del
 # #Função Fazer Backup
 # def fazer_backup
--- principal.py
+++ principal.py
@@ -1,4 +1,3 @@
 import funcoes
 funcoes.cabecalho()
 funcoes.menu()
-funcoes.cadastrar()
